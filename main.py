48231aa2cca3a382123db50a536dd9d17b890a85
Skoroye <suoemikoroye@gmail.com>
2023-01-18 08:24:40 -0600

    requirements update with psycopg2-binary for postgres, and main.py env vars  updates

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from functools import wraps
 
 from flask import Flask, render_template, redirect, request, url_for, flash, abort
@@ -18,13 +19,13 @@ from flask_gravatar import Gravatar
 db = SQLAlchemy()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY']    = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI']   = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']    = False
 # db = SQLAlchemy(app)
 
 db.init_app(app)
